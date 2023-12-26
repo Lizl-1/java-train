e50b7376495960d087344a5f7750801d48653772
lizl <237743663@qq.com>
2023-12-26 12:13:51 +0800

    更改

---
+++ src/main/java/com/zghc/train/TestController.java
@@ -0,0 +1,18 @@
+package com.zghc.train;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author Lizl
+ * @Description
+ * @Data 2023/12/26/0026 11:14
+ * @Version 1.0.0
+ */
+@RestController
+public class TestController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello world";
+    }
+}
