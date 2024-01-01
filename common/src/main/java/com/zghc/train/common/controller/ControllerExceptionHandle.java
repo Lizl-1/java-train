48e20efaaef3957a733e9a6fa0fcc58a705a0f96
lizl <237743663@qq.com>
2024-01-01 19:00:49 +0800

    为项目增加统一异常处理

---
+++ common/src/main/java/com/zghc/train/common/controller/ControllerExceptionHandle.java
@@ -0,0 +1,67 @@
+package com.zghc.train.common.controller;
+
+import com.zghc.train.common.exception.BusinessException;
+import com.zghc.train.common.resp.CommonResp;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @Author Lizl
+ * @Description
+ * @Data 2024/01/01/0001 15:17
+ * @Version 1.0.0
+ */
+@ControllerAdvice
+public class ControllerExceptionHandle {
+    private static final Logger LOG = LoggerFactory.getLogger(ControllerExceptionHandle.class);
+
+    /**
+     * 所有异常统一处理
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(value = Exception.class)
+    @ResponseBody
+    public CommonResp exceptionHandler(Exception e) {
+        CommonResp commonResp = new CommonResp();
+        LOG.error("系统异常：", e);
+        commonResp.setSuccess(false);
+//        commonResp.setMessage("系统出现异常，请联系管理员");
+        commonResp.setMessage(e.getMessage());
+        return commonResp;
+    }
+
+    /**
+     * 业务异常统一处理
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(value = BusinessException.class)
+    @ResponseBody
+    public CommonResp exceptionHandler(BusinessException e) {
+        CommonResp commonResp = new CommonResp();
+        LOG.error("业务异常：{}", e.getE().getDesc());
+        commonResp.setSuccess(false);
+        commonResp.setMessage(e.getE().getDesc());
+        return commonResp;
+    }
+
+    /**
+     * 校验异常统一处理
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(value = BindException.class)
+    @ResponseBody
+    public CommonResp exceptionHandler(BindException e) {
+        CommonResp commonResp = new CommonResp();
+        LOG.error("校验异常：{}", e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
+        commonResp.setSuccess(false);
+        commonResp.setMessage(e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
+        return commonResp;
+    }
+}
